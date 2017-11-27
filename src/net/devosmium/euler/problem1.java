0fc7fbc19b1c48aa1c9e65c7d8f78f8226114731
Oatmas64134 <Oatmas64134@gmail.com>
2017-11-27 19:39:31 +0000

    Start Work on Problem 1, https://projecteuler.net/problem=1

---
+++ src/net/devosmium/euler/problem1.java
@@ -0,0 +1,16 @@
+package net.devosmium.euler;
+/*
+ * Problem Title: Multiples of 3 and 5
+ * Problem URL: https://projecteuler.net/problem=1
+ * I certify that this work is my own. ~Dev_Osmium
+ */
+public class problem1 {
+    public static void solution() {
+    for (int i=0; i<1001; i++) {
+        int divby3 = i/3;
+        int divby5 = i/5;
+    }
+    }
+
+
+}
